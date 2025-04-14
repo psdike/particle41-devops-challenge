869f0c196e004c53486f2560481e54e8413c0804
Purushottam Dike <pdike11@gmail.com>
2025-04-14 21:08:50 +0530

    Created main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,12 @@
-# terraform/main.tf
-
 provider "aws" {
   region = var.aws_region
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
+  version = "5.1.0"
 
-  name = var.vpc_name
+  name = var.project_name
   cidr = var.vpc_cidr
 
   azs             = var.availability_zones
@@ -16,87 +14,63 @@ module "vpc" {
   private_subnets = var.private_subnets
 
   enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_dns_hostnames = true
 
   tags = {
-    Environment = var.env
+    Name = var.project_name
   }
 }
 
 module "ecs_cluster" {
   source  = "terraform-aws-modules/ecs/aws"
-  version = "4.1.2"
+  version = "5.6.0"
 
-  name = var.ecs_cluster_name
+  cluster_name = "${var.project_name}-cluster"
   capacity_providers = ["FARGATE"]
-  create_capacity_providers = false
 }
 
 resource "aws_ecs_task_definition" "app" {
-  family                   = "simple-time-service"
+  family                   = "${var.project_name}-task"
   requires_compatibilities = ["FARGATE"]
-  network_mode            = "awsvpc"
-  cpu                     = "256"
-  memory                  = "512"
-  execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
-  container_definitions   = jsonencode([
-    {
-      name      = "simpletimeservice"
-      image     = var.container_image
-      portMappings = [
-        {
-          containerPort = 8080
-          hostPort      = 8080
-        }
-      ]
-    }
-  ])
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  execution_role_arn       = aws_iam_role.ecs_task_exec.arn
+
+  container_definitions = jsonencode([{
+    name      = "simpletimeservice"
+    image     = var.container_image
+    portMappings = [{
+      containerPort = 8080
+      hostPort      = 8080
+    }]
+    essential = true
+  }])
 }
 
-resource "aws_ecs_service" "app" {
-  name            = "simpletimeservice"
-  cluster         = module.ecs_cluster.ecs_cluster_id
-  task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
+resource "aws_iam_role" "ecs_task_exec" {
+  name = "${var.project_name}-ecs-exec"
 
-  network_configuration {
-    subnets         = module.vpc.private_subnets
-    assign_public_ip = false
-    security_groups  = [aws_security_group.app_sg.id]
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.app_tg.arn
-    container_name   = "simpletimeservice"
-    container_port   = 8080
-  }
-  depends_on = [aws_lb_listener.http]
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
+  role       = aws_iam_role.ecs_task_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_security_group" "app_sg" {
-  name        = "app-sg"
-  description = "Allow inbound access from ALB"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "alb_sg" {
-  name        = "alb-sg"
-  description = "Allow HTTP access"
+  name        = "${var.project_name}-app-sg"
+  description = "Allow HTTP"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
@@ -114,56 +88,64 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
-resource "aws_lb" "app_alb" {
-  name               = "app-alb"
-  internal           = false
+module "lb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "8.7.0"
+
+  name            = "${var.project_name}-alb"
   load_balancer_type = "application"
-  subnets            = module.vpc.public_subnets
-  security_groups    = [aws_security_group.alb_sg.id]
-}
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.public_subnets
 
-resource "aws_lb_target_group" "app_tg" {
-  name     = "app-tg"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
-  target_type = "ip"
-  health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200"
-  }
-}
+  security_groups = [aws_security_group.app_sg.id]
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.app_alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
-
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
+  target_groups = [
+    {
+      name_prefix      = "simple"
+      backend_protocol = "HTTP"
+      backend_port     = 8080
+      target_type      = "ip"
+      health_check = {
+        path                = "/"
+        matcher             = "200"
+        interval            = 30
+        timeout             = 5
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
       }
-    ]
-  })
+    }
+  ]
+
+  listeners = [
+    {
+      port     = 80
+      protocol = "HTTP"
+      default_action = {
+        type             = "forward"
+        target_group_index = 0
+      }
+    }
+  ]
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+resource "aws_ecs_service" "app_service" {
+  name            = "${var.project_name}-service"
+  cluster         = module.ecs_cluster.cluster_id
+  launch_type     = "FARGATE"
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = module.vpc.private_subnets
+    assign_public_ip = false
+    security_groups = [aws_security_group.app_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = module.lb.target_groups[0].arn
+    container_name   = "simpletimeservice"
+    container_port   = 8080
+  }
+
+  depends_on = [module.lb]
 }
